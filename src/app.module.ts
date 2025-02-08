e81825d98c18176e01440a659576eadc821d7eab
unknown <jebonne12@gmail.com>
2025-02-08 13:50:28 +0800

    updated .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://jebonne:taroy123@bon.iw2hm.mongodb.net/'),
+    ConfigModule.forRoot({ isGlobal: true }), // Load .env globally
+    MongooseModule.forRoot(process.env.MONGODB_URI  || ''), // Use the env variable
     PostsModule,
   ],
 })
