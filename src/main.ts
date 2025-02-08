d1205f9e73847b661ed4ec6b4fca553f0f802826
unknown <jebonne12@gmail.com>
2025-02-08 15:15:43 +0800

    Updated cors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsMiddleware } from './cors.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    // Enable CORS
-    app.enableCors({
-      origin: ['http://localhost:5174', 'https://reddit-clone-concept.vercel.app'],// Allow frontend to access API
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true, // Allow cookies or authentication headers
-    });
+  // Use CORS middleware
+  app.use(new CorsMiddleware().use);
 
+  // Enable CORS explicitly
+  app.enableCors({
+    origin: 'https://reddit-clone-concept.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
