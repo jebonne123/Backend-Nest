82e4f1c4e2aaa873f2c0217834335652e9717352
unknown <jebonne12@gmail.com>
2025-02-08 16:03:26 +0800

    hehe

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,7 @@ async function bootstrap() {
     app.enableCors({
         origin: ['http://localhost:5174', '*'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
+        credentials: false,
     });
     await app.listen(process.env.PORT ?? 3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
     credentials: false,
   });
 
