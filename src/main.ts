6ae591c761b392d7993de2879ffc6fce85716a6d
unknown <jebonne12@gmail.com>
2025-02-08 15:42:09 +0800

    tabang

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: (origin, callback) => {
+      if (!origin || ['http://localhost:5174', 'https://reddit-clone-concept.vercel.app'].includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    credentials: false,
   });
 
   await app.listen(process.env.PORT || 3000);
